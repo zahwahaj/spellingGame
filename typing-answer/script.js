ccbbc988205fb424c10d5f08be5dd17bbc64ad60
Zahwa <zhajyousif@gmail.com>
2017-08-04 14:14:07 +0000

    5th commit with a few error

--- typing-answer/script.js
+++ typing-answer/script.js
@@ -5,15 +5,18 @@ document.head.appendChild(imported)
 words.sort(function(a, b) { return a.length - b.length });
 
 var myRec = new p5.SpeechRec();
-//myRec.continuous = true;
+myRec.continuous = true;
 //myRec.interimResults = true
 
 var foo = new p5.Speech();
 foo.setRate(.8)
 var randW;
-var word;
+var word = words[randW];
 var ticker = 0;
 
+// function score(){
+    
+// }
 
 function randomWord() {
     console.log("go")
@@ -22,9 +25,19 @@ function randomWord() {
     console.log(words[randW])
     foo.speak(words[randW])
     foo.onStart = hit()
-    word = words[randW]
-    // myRec.onResult = checkLetterSpeech;
-    //  myRec.start()
+   // word = words[randW]
+    myRec.onResult = checkLetterSpeech;
+    myRec.start()
+    return words[randW]
+}
+
+function newRandWord()
+{
+    randW = Math.floor((Math.random() * 110100) + 133)
+    console.log(words[randW])
+    foo.speak(words[randW])
+    //word = words[randW]
+    myRec.onResult = checkLetterSpeech()
     return words[randW]
 }
 
@@ -38,7 +51,7 @@ function repeat() {
     foo.speak(word)
 }
 // have the string get one of the random words from the program
-var string = randomWord()
+var string = randomWord().toLowerCase()
 
 // split the string into its multiple characters
 var wordToSpell = string.split("")
@@ -46,6 +59,7 @@ var wordToSpell = string.split("")
 // begin with the first letter in the chosen word to spell
 var letter = 0
 
+var scorenow = 0
 
 function submitLetter() {
     // have the computer read the letter that the user typed in
@@ -80,7 +94,7 @@ function submitLetter() {
             var trial = document.createElement("div")
             trial.setAttribute("id", "trial")
             document.body.appendChild(trial)
-            
+
             // prints a congrats message on the screen
             var header2 = document.createElement("H2")
             var congratsMessage = document.createTextNode("Congratulations! You're a great speller!");
@@ -97,6 +111,13 @@ function submitLetter() {
             congratsgif.style.height = "450px"
             congratsgif.style.marginLeft = "43%"
             trial.appendChild(congratsgif);
+            
+            // //count this as an increased score
+            // var myscore = document.getElementById("actualscore")
+            // var currentscore = document.createTextNote(scorenow)
+            // currentscore.appendChild(myscore)
+            // scorenow++
+            // console.log(scorenow)
 
             // get a next word button to appear
             var nextword = document.createElement("button")
@@ -114,6 +135,7 @@ function submitLetter() {
 
         // stop the user from typing more
         document.getElementById('input').disabled = true;
+        
 
         // a place to put everything will appear
         var trial = document.createElement("div")
@@ -155,47 +177,144 @@ function newTry() {
     console.log("we're done")
     var trial = document.getElementById('trial')
     trial.parentNode.removeChild(trial);
-    string = randomWord()
+    string = newRandWord()//newRandWord
     console.log(string + " your new string")
     document.getElementById('input').disabled = false;
     letter = 0
 }
 
 
-function showResult() {
-    if (myRec.resultValue == true) {
-        //background(192, 255, 192);
-        //text(myRec.resultString, width/2, height/2);
-        console.log(myRec.resultString);
+// function showResult() {
+//     if (myRec.resultValue == true) {
+//         //background(192, 255, 192);
+//         //text(myRec.resultString, width/2, height/2);
+//         console.log(myRec.resultString);
+//     }
+// }
+
+
+function checkLetterSpeech() {
+    console.log("this is what you said " + myRec.resultString)
+    console.log("this is the index of the letter " + word[ticker+1]) //added + 1
+    if (myRec.resultString == "hey") {
+        myRec.resultString = "a"
+    }
+    if (myRec.resultString == "bee") {
+        myRec.resultString = "b"
+    }
+    if (myRec.resultString == "be") {
+        myRec.resultString = "b"
+    }
+    if (myRec.resultString == "see") {
+        myRec.resultString = "c"
+    }
+    if (myRec.resultString == "sea") {
+        myRec.resultString = "c"
+    }
+    if (myRec.resultString == "earth") {
+        myRec.resultString = "f"
+    }
+    if (myRec.resultString == "eye") {
+        myRec.resultString = "i"
+    }
+    if (myRec.resultString == "jay") {
+        myRec.resultString = "j"
+    }
+    if (myRec.resultString == "kay") {
+        myRec.resultString = "k"
+    }
+    if (myRec.resultString == "el") {
+        myRec.resultString = "l"
+    }
+    if (myRec.resultString == "hello") {
+        myRec.resultString = "l"
+    }
+    if (myRec.resultString == "oh") {
+        myRec.resultString = "o"
+    }
+    if (myRec.resultString == "pee") {
+        myRec.resultString = "p"
+    }
+    if (myRec.resultString == "queue") {
+        myRec.resultString = "q"
+    }
+    if (myRec.resultString == "are") {
+        myRec.resultString = "r"
+    }
+    if (myRec.resultString == "tea") {
+        myRec.resultString = "t"
+    }
+    if (myRec.resultString == "tee") {
+        myRec.resultString = "t"
+    }
+    if (myRec.resultString == "you") {
+        myRec.resultString = "u"
+    }
+    if (myRec.resultString == "double you") { myRec.resultString = "w" }
+    if (myRec.resultString == "why") { myRec.resultString = "y" }
+
+
+    if (myRec.resultString == word[ticker]) {//.toLowerCase()
+        ticker++
+        //myRec.onResult = myRec.start()
+        console.log(ticker)
+        console.log("listening again")
+    }
+
+    else {
+        console.log("u suck")
+    }
+
+    //words[randW].charAt(i)
+    //         console.log(myRec.resultString)
+    //         console.log("true")
+    //         myRec.start()
+    //         ticker++
+    //     else {
+    //         console.log("i am annoyed")
+    //     }
+
+    // }
+    if(ticker == word.length)
+    {
+         // stop the user from typing more
+            document.getElementById('input').disabled = true;
+
+            var trial = document.createElement("div")
+            trial.setAttribute("id", "trial")
+            document.body.appendChild(trial)
+
+            // prints a congrats message on the screen
+            var header2 = document.createElement("H2")
+            var congratsMessage = document.createTextNode("Congratulations! You're a great speller!");
+            header2.style.textAlign = "center"
+            header2.style.fontFamily = 'Abril Fatface', 'cursive';
+            header2.style.fontSize = "60px";
+            header2.appendChild(congratsMessage);
+            trial.appendChild(header2);
+
+            //pop up a gif if you get the word correct!
+            var congratsgif = document.createElement("img")
+            congratsgif.setAttribute("src", "https://m.popkey.co/7aa178/kv4ol.gif");
+            congratsgif.style.width = "600px"
+            congratsgif.style.height = "450px"
+            congratsgif.style.marginLeft = "43%"
+            trial.appendChild(congratsgif);
+            
+            // //count this as an increased score
+            // var myscore = document.getElementById("actualscore")
+            // var currentscore = document.createTextNote(scorenow)
+            // currentscore.appendChild(myscore)
+            // scorenow++
+            // console.log(scorenow)
+
+            // get a next word button to appear
+            var nextword = document.createElement("button")
+            nextword.innerHTML = "Next Word"
+            nextword.className = 'button'
+            nextword.style.marginLeft = "45%"
+            nextword.setAttribute("onClick", "newTry()")
+            trial.appendChild(nextword)
+            //myRec.continuous = false;
     }
 }
-
-
-// function checkLetterSpeech() {
-//     console.log(myRec.resultString)
-//     console.log(word[ticker])
-//     if (myRec.resultString == "oh") {
-//         myRec.resultString = "o"
-//     }
-//     if (myRec.resultString.toLowerCase() == word[ticker]) {
-//         ticker++
-//         //myRec.onResult = myRec.start()
-//         console.log(ticker)
-//         console.log("listening again")
-//     }
-
-//     else {
-//         console.log("u suck")
-//     }
-
-//     //words[randW].charAt(i)
-//     //         console.log(myRec.resultString)
-//     //         console.log("true")
-//     //         myRec.start()
-//     //         ticker++
-//     //     else {
-//     //         console.log("i am annoyed")
-//     //     }
-
-//     // }
-// }
